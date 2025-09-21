8fee3a29e84d44b07e0e8c8d3145ef49bd5e6732
m3 <mlamont@gmail.com>
2025-09-21 18:27:04 -0400

    coding read-only functionality

--- bcn-index.js
+++ bcn-index.js
@@ -9,78 +9,80 @@ import { sepolia } from "https://esm.sh/viem/chains";
 import { contractAddress, abi } from "./bcn-constants.js";
 
 // grab website elements
-const topSpan = document.getElementById("topSpan");
-const greetingSpan = document.getElementById("greetingSpan");
+// const getNameOf76A923Button = document.getElementById("getNameOf76A923Button");
 const connectButton = document.getElementById("connectButton");
 const colorInput = document.getElementById("colorInput");
 const namedButton = document.getElementById("namedButton");
-const currentNameDiv = document.getElementById("currentNameDiv");
-const currentOwnerDiv = document.getElementById("currentOwnerDiv");
-const nameInput = document.getElementById("nameInput");
-const nameItButton = document.getElementById("nameItButton");
-const getNameOf76A923Button = document.getElementById("getNameOf76A923Button");
+const currentNameSpan = document.getElementById("currentNameSpan");
+const currentOwnerSpan = document.getElementById("currentOwnerSpan");
+const ownerIsNobodyDiv = document.getElementById("ownerIsNobodyDiv");
 
 // initialize viem pieces
 let walletClient;
 let publicClient;
 
-// keeping the connect() function in index-js.js for now
-
-async function getNameOf76A923() {
-  if (typeof window.ethereum !== "undefined") {
-    publicClient = createPublicClient({
-      chain: sepolia,
-      transport: http(),
-      //   transport: custom(window.ethereum),
-    });
-    console.log("public client created");
-
-    const result = await publicClient.readContract({
-      address: contractAddress,
-      abi: abi,
-      functionName: "getName",
-      args: ["76A923"],
-    });
-
-    console.log(result);
-    input.value = result;
-  } else {
-    connectButton.innerHTML = "Please install MetaMask!";
-  }
-}
+let tokenName;
+let tokenOwner;
 
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
-    topSpan.innerHTML = "";
-    publicClient = createPublicClient({
+    walletClient = createWalletClient({
       chain: sepolia,
       transport: http(),
     });
-    console.log("public client created from bcn-index.js");
+    await walletClient.requestAddresses();
+    console.log("wallet client created from connect() in bcn-index.js");
+    connectButton.innerHTML = "(connected)";
   } else {
-    topSpan.innerHTML = "...PLEASE INSTALL METAMASK!";
+    connectButton.innerHTML = "...PLEASE INSTALL METAMASK!";
   }
 }
 
 async function named() {
   if (typeof window.ethereum !== "undefined") {
-    topSpan.innerHTML = "";
-    //
+    publicClient = createPublicClient({
+      chain: sepolia,
+      transport: http(),
+    });
+    console.log("public client created from named() in bcn-index.js");
+    try {
+      tokenName = await publicClient.readContract({
+        address: contractAddress,
+        abi: abi,
+        functionName: "getName",
+        args: [colorInput.value],
+      });
+      tokenOwner = await publicClient.readContract({
+        address: contractAddress,
+        abi: abi,
+        functionName: "getOwner",
+        args: [colorInput.value],
+      });
+      currentNameSpan.innerHTML = tokenName;
+      currentOwnerSpan.innerHTML = tokenOwner;
+      ownerIsNobodyDiv.hidden = true;
+      ownerIsSomebodyDiv.hidden = false;
+    } catch (error) {
+      console.log(error.message);
+      if (error.message.includes("ERC721NonexistentToken")) {
+        currentNameSpan.innerHTML = "Up for grabs!";
+        currentOwnerSpan.innerHTML = "Could be you!";
+        ownerIsNobodyDiv.hidden = false;
+        ownerIsSomebodyDiv.hidden = true;
+      } else {
+        currentNameSpan.innerHTML = "please try...";
+        currentOwnerSpan.innerHTML = "...that again";
+        ownerIsNobodyDiv.hidden = true;
+        ownerIsSomebodyDiv.hidden = true;
+      }
+    }
   } else {
-    topSpan.innerHTML = "...PLEASE INSTALL METAMASK!";
+    currentNameSpan.innerHTML = "please first...";
+    currentOwnerSpan.innerHTML = "...install Metamask";
+    ownerIsNobodyDiv.hidden = true;
+    ownerIsSomebodyDiv.hidden = true;
   }
 }
 
-async function nameIt() {
-  if (typeof window.ethereum !== "undefined") {
-    topSpan.innerHTML = "";
-    //
-  } else {
-    topSpan.innerHTML = "...PLEASE INSTALL METAMASK!";
-  }
-}
-
-getNameOf76A923Button.onclick = getNameOf76A923;
-connectButton.onclick = connect();
-namedButton.onclick = named();
-nameItButton.onclick = nameIt();
+connectButton.onclick = connect;
+namedButton.onclick = named;
